8fae16408fdd99fb67095c78b20f4fdebcbd07c8
muzammilmzmlk <152701759+muzammilmzmlk@users.noreply.github.com>
2026-09-17 00:55:44 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="cashbook-v106";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"])).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{ if(e.request.method!=="GET") return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html")))); });
